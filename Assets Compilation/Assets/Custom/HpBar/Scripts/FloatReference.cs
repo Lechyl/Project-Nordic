710ebf13dec60d1cb57eb1d80f127bbbd56397bc
Nick <41940951+Invusius@users.noreply.github.com>
2020-08-06 10:57:34 +0200

    Hpbar
    HpBar med prefab

---
+++ Assets Compilation/Assets/Custom/HpBar/Scripts/FloatReference.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class FloatReference 
+{
+    public bool UseConstant = true;
+    public float ConstantValue;
+    public FloatVariable Variable; 
+
+    public float Value
+    {
+        get { return UseConstant ? ConstantValue : Variable.Value; }
+        set
+        {
+            if (UseConstant)
+                ConstantValue = value;
+            else
+                Variable.Value = value; 
+        }
+    }
+
+
+}
---
+++ Assets Compilation/Assets/Custom/HpBar/Scripts/PlayerHealth.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class PlayerHealth : ScriptableObject
+{
+
+    public float CurrentHp;
+    public float MaxHp;
+
+
+}
