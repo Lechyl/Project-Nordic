84cf0eda6e4a5d88e994ab9923a2de1c079a2f1d
Nick <41940951+Invusius@users.noreply.github.com>
2020-09-29 14:26:39 +0200

    merge again

--- Assets Compilation/Assets/Custom/Inventory/Scripts/InventorySlotController.cs
+++ Assets Compilation/Assets/Custom/Inventory/Scripts/InventorySlotController.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventorySlotController : MonoBehaviour
+public class InventorySlotController : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler
 {
     public InventoryStackItems stackItem;
     
@@ -11,24 +13,43 @@ public class InventorySlotController : MonoBehaviour
     {
       //  updateInfo();
     }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!string.IsNullOrEmpty(stackItem.item.itemName))
+        {
+            Inventory.instance.ChangeTooltipText(stackItem.item);
 
+            Inventory.instance.ShowTooltip(transform.position);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Inventory.instance.HideTooltip();
+
+    }
 
     public void UpdateInfo()
     {
         Text displayText = transform.GetChild(0).GetChild(0).GetComponent<Text>();
         Image displayImage = transform.GetChild(0).GetChild(1).GetComponent<Image>();
-       // Text displayText = transform.Find("Text").GetComponent<Text>();
-       // Image displayImage = transform.Find("Image").GetComponent<Image>();
-        if(!string.IsNullOrEmpty(stackItem.item.itemName))
+        TextMeshProUGUI displayStack = transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+
+        //TextMeshPro displayStack = transform.GetChild(2).GetComponent<TextMeshPro>();
+        // Text displayText = transform.Find("Text").GetComponent<Text>();
+        // Image displayImage = transform.Find("Image").GetComponent<Image>();
+        if (!string.IsNullOrEmpty(stackItem.item.itemName))
         {
 
             displayText.text = stackItem.item.itemName;
+            displayStack.text = stackItem.stack.ToString();
             displayImage.sprite = stackItem.item.icon;
             displayImage.color = Color.white;
         }
         else
         {
             displayText.text = "";
+            displayStack.text = "";
             displayImage.sprite = null;
             displayImage.color = Color.clear;
         }
@@ -36,6 +57,4 @@ public class InventorySlotController : MonoBehaviour
     }
 
 
-  
-    
 }
